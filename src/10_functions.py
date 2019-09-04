9307de616f07a5e3f5904741e6435f930199cc1a
HuMart <manya01@protonmail.com>
2019-09-04 10:38:10 -0400

    functions.py

--- src/10_functions.py
+++ src/10_functions.py
@@ -10,3 +10,13 @@ num = int(num)
 
 # YOUR CODE HERE
 
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    elif n % 2 == 1:
+        return False
+
+if is_even(num):
+    print("Even")
+elif not is_even(num):
+    print("Odd")
